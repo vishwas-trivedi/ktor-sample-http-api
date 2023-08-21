92514749ed26849c47362852ab56fd2fcbc2918e
Vishwas Trivedi <36479242+vishwas-trivedi@users.noreply.github.com>
2023-08-21 13:39:44 +0900

    - Added controller to fetch all cusotmer list

--- src/main/kotlin/com/example/Controllers/CustomerController.kt
+++ src/main/kotlin/com/example/Controllers/CustomerController.kt
@@ -1,5 +1,9 @@
 package com.example.Controllers
 
+import com.example.models.customerStorage
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Route.customerController() {
@@ -8,7 +12,12 @@ fun Route.customerController() {
          * Get list of all the customers
          */
         get {
-
+            if (customerStorage.isNotEmpty()){
+                call.respond(customerStorage)
+            }
+            else{
+                call.respondText(text="No customer found", status= HttpStatusCode.OK)
+            }
         }
         /**
          * Get detail of specific customer
